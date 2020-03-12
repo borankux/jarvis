d26be3e6bd00352524443e9bbaf2a6e4f3540431
Frank <mablat@ebay.com>
2020-03-12 14:55:54 +0800

    add view

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,8 @@ Route::get('/', function () {
         'version' => '1.0'
     ]);
 });
+
+Route::get('/projects', function(){
+    $projects = \App\Model\Project::all();
+    return view('projects', compact('projects'));
+});
