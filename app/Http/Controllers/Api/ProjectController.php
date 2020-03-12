95abde425a99395c765df0cef62171edf94946c3
Frank <mablat@ebay.com>
2020-03-12 13:05:01 +0800

    abstract service

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\Api;
 
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        
+    }
 }
